503e3b57b73ce531d2284d9d6459dc8d137177dc
Chandni <sharma.cha@husky.neu.edu>
2017-06-16 00:15:40 -0400

    Update Linear_Model.py

--- DeepLearningWithTensorflow/TensorFlowExercises/Linear_Model.py
+++ DeepLearningWithTensorflow/TensorFlowExercises/Linear_Model.py
@@ -7,3 +7,8 @@ init = tf.global_variable_initialized()
 sess=tf.Session()
 sess.run(init)
 print(sess.run(linear_model, {x:[1,2,3,4]}))
+y = tf.placeholder(tf.float32)
+squared_deltas=tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
